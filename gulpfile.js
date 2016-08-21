23d526e778a46544c873f6997615c960b3f2c51e
Dinesh Bogolu <visitdinu@gmail.com>
2016-08-22 01:06:02 +0530

    Add gulpfile.js
    Add below gulp tasks:
    - connect
    - open
    - html

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+//runs a local dev server
+var connect = require('gulp-connect');
+//opens a URL in a web browser
+var open = require('gulp-open');
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*/html',
+        build: './build'
+    }
+};
+
+//Start a local dev server
+gulp.task('connect', function () {
+    connect.server({
+        root: ['build'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function () {
+    gulp.src('dist/index.html')
+        .pipe(open({
+            uri: config.devBaseUrl + ':' + config.port + '/'
+        }));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.build))
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'open']);
+
