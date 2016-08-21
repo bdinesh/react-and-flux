d921a79bf32a0bdb044bfe2a7ad30f4d8205991a
Dinesh Bogolu <visitdinu@gmail.com>
2016-08-22 01:53:53 +0530

    Update gulp tasks
    - Add js gulp task
    - Update watch task to watch js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 'use strict';
 
 var gulp = require('gulp');
-//runs a local dev server
-var connect = require('gulp-connect');
-//opens a URL in a web browser
-var open = require('gulp-open');
+var connect = require('gulp-connect'); //runs a local dev server
+var open = require('gulp-open'); //opens a URL in a web browser
+var browserify = require('browserify'); //Bundles JS files
+var reactify = require('reactify'); //Transpiles JSX to JS
+var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp 
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        build: './build'
+        js: './src/**/*.js',
+        build: './build',
+        mainJs: './src/main.js'
     }
+
 };
 
 //Start a local dev server
@@ -39,9 +43,20 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.build + '/scripts'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
 
