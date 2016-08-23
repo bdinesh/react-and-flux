c17e9f3b3f7a1a67c41e61dc4662032c64124371
Dinesh Bogolu <visitdinu@gmail.com>
2016-08-24 00:03:15 +0530

    Update gulp tasks
    - Add 'clean:build' task
    - Replace reactify with babelify for transpiling JSX

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ var connect = require('gulp-connect'); //runs a local dev server
 var open = require('gulp-open'); //opens a URL in a web browser
 var browserify = require('browserify'); //Bundles JS files
 var reactify = require('reactify'); //Transpiles JSX to JS
+var babelify = require('babelify');
 var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp 
 var concat = require('gulp-concat'); // Concatinates files
 var eslint = require('gulp-eslint'); //lints JS files and JSX
+var del = require('del');
 
 var config = {
     port: 9005,
@@ -48,17 +50,19 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
-gulp.task('js', function(){
-    browserify(config.paths.mainJs)
-        .transform(reactify)
+gulp.task('js', function () {
+    browserify(config.paths.mainJs, {
+        debug: true
+    })
+        .transform(babelify)
         .bundle()
         .on('error', console.error.bind(console))
         .pipe(source('bundle.js'))
         .pipe(gulp.dest(config.paths.build + '/scripts'))
         .pipe(connect.reload());
-}); 
-    
-gulp.task('css', function(){      
+});
+
+gulp.task('css', function () {
     gulp.src(config.paths.css)
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest(config.paths.build + '/css'));
@@ -69,9 +73,7 @@ gulp.task('watch', function () {
     gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
-
-gulp.task('lint', function(){
+gulp.task('lint', function () {
     return gulp.src(config.paths.js)
         .pipe(eslint({
             config: '.eslintrc.json'
@@ -79,3 +81,10 @@ gulp.task('lint', function(){
         .pipe(eslint.format());
 });
 
+gulp.task('clean:build', function () {
+    return del([
+        'build/**/*'
+    ]);
+});
+
+gulp.task('default', ['clean:build', 'html', 'js', 'css', 'lint', 'open', 'watch']);
